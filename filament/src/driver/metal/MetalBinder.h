727be1c8bc2a0c7e11f16a6d7ce3a80de7004816
Benjamin Doherty <bendoherty@google.com>
2018-11-26 15:50:57 -0800

    Fix SAMPLER_COUNT todo

--- filament/src/driver/metal/MetalBinder.h
+++ filament/src/driver/metal/MetalBinder.h
@@ -40,9 +40,7 @@ static constexpr uint32_t VERTEX_BUFFER_START = BindingPoints::COUNT;
 static constexpr uint32_t MAX_VERTEX_ATTRIBUTES = filament::ATTRIBUTE_INDEX_COUNT;
 static constexpr uint32_t NUM_UBUFFER_BINDINGS = filament::BindingPoints::COUNT;
 static constexpr uint32_t SAMPLER_BINDINGS_START = NUM_UBUFFER_BINDINGS;
-
-// todo: saw this in VulkanBinder, but how do we know this?
-static constexpr uint32_t NUM_SAMPLER_BINDINGS = 8;
+static constexpr uint32_t NUM_SAMPLER_BINDINGS = filament::MAX_SAMPLER_COUNT;
 
 struct MetalBinderImpl;
 
